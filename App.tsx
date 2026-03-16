b2546ce3c2c76601395fff6e3e14250eb27559b7
HunterAshryver <Guilherme.meira9572@gmail.com>
2026-03-16 12:47:18 -0300

    Melhorei a interface deixando ela mais bonita e funcional

--- App.tsx
+++ App.tsx
@@ -33,38 +33,85 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>
-        Sistema de Monitoramento de Sensores
-      </Text>
+      <Text style={styles.titulo}>Sistema de Monitoramento de Sensores </Text>
+
       <View style={styles.card}>
-        <Text>Sensor: {medicao.sensor}</Text>
-        <Text>Tipo: {medicao.tipo}</Text>
-        <Text>Valor: {medicao.valor}</Text>
-        <Text>Status: {medicao.status}</Text>
+        <Text style={styles.label}>Sensor: </Text>
+        <Text style={styles.valor}> {medicao.sensor}</Text>
+
+        <Text style={styles.label}>Tipo: </Text>
+        <Text style={styles.valor}> {medicao.tipo}</Text>
+
+        <Text style={styles.label}>Valor: </Text>
+        <Text style={styles.valor}> {medicao.valor}</Text>
+
+        <Text style={styles.label}>Status: </Text>
+        <Text style={[styles.valor, styles.status [medicao.status]]}> 
+          {medicao.status.toUpperCase()}
+          </Text>
+
+        <View style={styles.botao}>
+
         <Button
           title="Simular Nova Medição"
           onPress={simularNovaMedicao}
+          color="#231ea6"
         />
+        </View>
       </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
-    backgroundColor: "#ffffff",
+    backgroundColor: "#212123",
     alignItems: "center",
     justifyContent: "center",
   },
+
   titulo: {
+    color: "#eaeaea",
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+    textAlign: "center",
   },
+  
   card: {
-    width: "80%",
-    padding: 20,
+    width: "90%",
+    padding: 24,
     borderWidth: 1,
-    borderRadius: 8,
+    borderRadius: 16,
+    borderColor: "#787af8",
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+
+    botao: {
+      marginTop: 20,
+  },
+
+    label: {
+      fontSize: 16,
+      color: "#AAAAAA",
+      marginTop: 12,
+      marginBottom: 4,
+  },
+
+    valor: {
+      fontSize: 18,
+      color: "#FFFFFF",
+      fontWeight: "500",
+  },
+
+    status: {
+      normal: { color: "#4fb953" },
+      alerta: { color: "#FF9800" },
+      critico: { color: "#F44336" },
   },
 });
